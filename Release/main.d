b1ab3dd159f80e429a4ad3a8db1578899fd56e72
Fidhi Pradinni <fidhipradinni28@gmail.com>
2019-12-12 15:59:13 -0800

    Bismillah project baru

---
+++ Release/main.d
@@ -0,0 +1,19 @@
+main.d: ../main.c ../myADC/myADC.h ../mySPI/mySPI.h ../myUART/myUART.h \
+ ../mySmartSensor/mySmartSensor.h \
+ C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\myADC/myADC.h \
+ C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\myUART/myUART.h \
+ C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\mySPI/mySPI.h
+
+../myADC/myADC.h:
+
+../mySPI/mySPI.h:
+
+../myUART/myUART.h:
+
+../mySmartSensor/mySmartSensor.h:
+
+C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\myADC/myADC.h:
+
+C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\myUART/myUART.h:
+
+C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\mySPI/mySPI.h:
---
+++ Release/mySPI/mySPI.d
@@ -0,0 +1,6 @@
+mySPI/mySPI.d: ../mySPI/mySPI.c ../mySPI/mySPI.h \
+ C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\myADC/myADC.h
+
+../mySPI/mySPI.h:
+
+C:\Users\Fidhi\ Pradini\workspace\WML_SmartSensor_Slave-master\myADC/myADC.h:
